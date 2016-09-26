6ed0ac03331990a05010fd8d9504b5ae7bc19c3c
Alex Schin <schinalex@gmail.com>
2016-09-26 16:37:17 +0300

    added ncinames for known IPs

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
+'use strict'
+
 const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
-
+const names = {
+  '87.255.84.77': 'Dorin',
+  '193.33.93.43': 'Alex',
+  '95.65.36.159': 'Andy',
+  '127.0.0.1': 'Home'
+}
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
 io.on('connection', (socket) => {
-  console.log('a user connected')
-  var address = socket.request.socket.remoteAddress.split(':').pop()
+  let ip = socket.request.socket.remoteAddress.split(':').pop()
+  console.log('+ ' + (names[ip] || ip) + ' connected: '+ ip)
 
   socket.on('disconnect', () => {
-   console.log('user disconnected')
+   console.log('- ' + (names[ip] || ip) + ' disconnected: ' + ip)
   })
 
   socket.on('chat message', (msg) => {
-    console.log(address + '> ' + msg)
-    io.emit('chat message', address + '> ' + msg)
+    console.log((names[ip] || ip) + '> ' + msg)
+    io.emit('chat message', (names[ip] || ip)+ '> ' + msg)
   })
 })
 
