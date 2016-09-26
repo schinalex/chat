7bf5f7535ae27cd41803ec9ee8bb51930e9aac91
Alex Schin <schinalex@gmail.com>
2016-09-26 16:09:28 +0300

    deleted comment console.logs

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ io.on('connection', (socket) => {
   })
 
   socket.on('chat message', (msg) => {
-    console.log(typeof msg)
-    console.log(address)
     console.log(address + '> ' + msg)
     io.emit('chat message', address + '> ' + msg)
   })
